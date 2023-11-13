5a2f2ca50a33bbe57e24a979f2618340a3fa830f
jindamgom <update_set@naver.com>
2023-11-13 11:46:47 +0900

    main#동물기능 시작 출력문 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -13,6 +13,7 @@ public class Main
 		System.out.println("HELLO WORLD"); //ctrl+shift+x
 		System.out.println("HELLO GIT");
 		
+		System.out.println("동물기능 시작");
 		//sub 클래스 제거
 		//dog 클래스 bark 호출
 		new Dog().bark();
