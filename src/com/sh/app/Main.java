473d7e6676b6228919f3fa5dc5fd84942c1a5006
jindamgom <update_set@naver.com>
2023-11-13 12:12:30 +0900

    cat 관련 기능 제거

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,7 +1,6 @@
 package com.sh.app;
 
 import com.sh.app.animal.Bird;
-import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
 import com.sh.app.animal.Fish;
 import com.sh.app.animal.Mouse;
@@ -20,10 +19,7 @@ public class Main
 		//sub 클래스 제거
 		//dog 클래스 bark 호출
 		new Dog().bark();
-		
-		//cat 클래스 jump 호출
-		new Cat().jump();
-		
+
 		//branch - animal bird 클래스 fly 호출
 		new Bird().fly();
 		
@@ -39,5 +35,6 @@ public class Main
 	public void test()
 	{
 		System.out.println("test");
+
 	}
 }
--- src/com/sh/app/animal/Cat.java
+++
@@ -1,9 +0,0 @@
-package com.sh.app.animal;
-
-public class Cat 
-{
-	public void jump()
-	{
-		System.out.println("점프😸");
-	}
-}
